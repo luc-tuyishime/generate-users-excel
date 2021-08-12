f0e55ca3a43c8919f61d7e5f49cb6ff6612ec32e
luc-tuyishime <luctunechi45@gmail.com>
2021-08-13 00:49:34 +0200

    update validation part

--- src/helpers/validation/joi-schemas/userColumns.js
+++ src/helpers/validation/joi-schemas/userColumns.js
@@ -2,8 +2,8 @@ import Joi from '@hapi/joi';
 import { validateEmail } from './newUser';
 
 export const schemaValidation = Joi.object({
-    names: Joi.string().min(3).max(15).required().label('Names'),
-    nid: Joi.string().length(36).required().label('National ID'),
+    names: Joi.string().min(3).max(15).required(),
+    nid: Joi.string().length(36).required(),
     phone: Joi.string()
         .length(13)
         .pattern(/^[0-9\-\+]{9,15}$/)
