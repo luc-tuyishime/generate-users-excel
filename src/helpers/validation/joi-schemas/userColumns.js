54ed16d4f32b2a9cd586403f3caba756f8d950ec
luc-tuyishime <luctunechi45@gmail.com>
2021-08-13 08:38:11 +0200

    add default value of empty

--- src/helpers/validation/joi-schemas/userColumns.js
+++ src/helpers/validation/joi-schemas/userColumns.js
@@ -2,15 +2,8 @@ import Joi from '@hapi/joi';
 import { validateEmail } from './newUser';
 
 export const schemaValidation = Joi.object({
-    names: Joi.string()
-        .min(3)
-        .max(15)
-        .required()
-        .regex(/[$\(\)<>]/, { invert: true }),
-    nid: Joi.string()
-        .length(36)
-        .required()
-        .regex(/[$\(\)<>]/, { invert: true }),
+    names: Joi.string().min(3).max(15).empty('').default('Empty value'),
+    nid: Joi.string().length(36).required(),
     phone: Joi.string()
         .length(13)
         .pattern(/^[0-9\-\+]{9,15}$/)
@@ -20,15 +13,7 @@ export const schemaValidation = Joi.object({
             'object.regex': 'Should be Numbers',
             'string.pattern.base':
                 'Must be a valid phone number Ex: (+250788888888)',
-        })
-        .required()
-        .regex(/[$\(\)<>]/, { invert: true }),
-    gender: Joi.string()
-        .valid('Male', 'Female')
-        .required()
-        .regex(/[$\(\)<>]/, { invert: true }),
-    email: Joi.string()
-        .custom(validateEmail)
-        .required()
-        .regex(/[$\(\)<>]/, { invert: true }),
+        }),
+    gender: Joi.string().valid('Male', 'Female'),
+    email: Joi.string().custom(validateEmail),
 });
