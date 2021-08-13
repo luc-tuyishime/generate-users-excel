43f0ae5b9c06ab5b2a84e4a5c22bb105bdf694dc
luc-tuyishime <luctunechi45@gmail.com>
2021-08-13 08:10:17 +0200

    fix validation for data

--- src/helpers/validation/joi-schemas/userColumns.js
+++ src/helpers/validation/joi-schemas/userColumns.js
@@ -2,27 +2,18 @@ import Joi from '@hapi/joi';
 import { validateEmail } from './newUser';
 
 export const schemaValidation = Joi.object({
-    names: Joi.string()
-        .min(3)
-        .max(15)
-        .required()
-        .regex(/[$\(\)<>]/, { invert: true }),
+    names: Joi.string().min(3).max(15),
     nid: Joi.string().length(36).required(),
     phone: Joi.string()
         .length(13)
         .pattern(/^[0-9\-\+]{9,15}$/)
         .required()
-        .regex(/[$\(\)<>]/, { invert: true })
         .messages({
             'string.min': 'Should Start with + character',
             'object.regex': 'Should be Numbers',
             'string.pattern.base':
                 'Must be a valid phone number Ex: (+250788888888)',
         }),
-    gender: Joi.string()
-        .valid('Male', 'Female')
-        .regex(/[$\(\)<>]/, { invert: true }),
-    email: Joi.string()
-        .custom(validateEmail)
-        .regex(/[$\(\)<>]/, { invert: true }),
+    gender: Joi.string().valid('Male', 'Female'),
+    email: Joi.string().custom(validateEmail),
 });
